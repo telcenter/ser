136edf5500b7172470155d31435d6d35372728a1
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-05-03 15:37:58 +0700

    feat: refactor + package as library

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
-from model import create_model, load_model
-from extract_features import get_predict_feat
-from encoder import create_encoder
-import numpy as np
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-import os
-import time
-
-emotion_map = {
-    0: 'neutral',
-    1: 'calm',
-    2: 'happy',
-    3: 'sad', 
-    4: 'angry',
-    5: 'fear',
-    6: 'disgust'
-}
-
-def predict_emotion(path, model, encoder):
-    res = get_predict_feat(path)
-    predictions = model.predict(res)
-    print(f"pred: {predictions}, max: {np.argmax(predictions)}")
-    y_pred = encoder.inverse_transform(predictions)
-    print(y_pred[0][0])
-    return y_pred[0][0]
+from ser import SERModel
 
 def main():
-    start_time = time.time()
-    # model = create_model('./model_weight/best_model1_weights.h5')
-    model = load_model('./CNN_model.json','./model_weight/best_model1_weights.h5')
-    audio_path = './test_data/angry-short.wav'
-    encoder = create_encoder()
-    emotion = predict_emotion(audio_path, model, encoder)
+    model = SERModel(
+        model_path='./model_weight/CNN_model.json',
+        weights_path='./model_weight/best_model1_weights.h5'
+    )
+    audio_path = './test_data/surprise.wav'
+    emotion = model.predict_emotion_from_wav_file(audio_path)
     print(f"Predicted emotion: {emotion}")
-    end_time = time.time()
-    print(f"Time taken: {end_time - start_time:.2f} seconds")
-
 
 if __name__ == "__main__":
     main()
---
+++ ser/__init__.py
@@ -0,0 +1,42 @@
+from model import load_model #, create_model
+from ser.extract_features import get_predict_feat
+from ser.encoder import create_encoder
+import numpy as np
+# from sklearn.preprocessing import StandardScaler, LabelEncoder
+# import os
+import time
+import logging
+
+# emotion_map = {
+#     0: 'neutral',
+#     1: 'calm',
+#     2: 'happy',
+#     3: 'sad', 
+#     4: 'angry',
+#     5: 'fear',
+#     6: 'disgust'
+# }
+
+class SERModel:
+    def __init__(self, model_path: str, weights_path: str):
+        """
+        Recommended files:
+        model_path: `./model_weight/CNN_model.json`
+        weights_path: `./model_weight/best_model1_weights.h5`
+        """
+        self.model = load_model(model_path, weights_path)
+        self.encoder = create_encoder()
+
+    def predict_emotion_from_wav_file(self, wav_file_path: str):
+        start_time = time.time()
+        try:
+            res = get_predict_feat(wav_file_path)
+            predictions = self.model.predict(res)
+            logging.info(f"SER: pred: {predictions}, max: {np.argmax(predictions)}")
+            y_pred = self.encoder.inverse_transform(predictions)
+            inference_result = y_pred[0][0]
+            logging.info(f"SER: inference result: {inference_result}")
+            return inference_result
+        finally:
+            end_time = time.time()
+            logging.info(f"SER: inference time: {end_time - start_time:.2f} seconds")
