ac3fe0a0f2ced1e44572582dc9fb7f6edcc145fe
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-05-07 15:21:09 +0700

    refactor

--- ser/__init__.py
+++ ser/__init__.py
@@ -4,7 +4,7 @@ from .encoder import create_encoder
 import numpy as np
 import time
 import logging
-
+from typing import Literal
 
 class SERModel:
     def __init__(self, model_path: str, weights_path: str):
@@ -16,27 +16,26 @@ class SERModel:
         self.model = load_model(model_path, weights_path)
         self.encoder = create_encoder()
 
-    def predict_emotion_from_wav_file(self, wav_file_path: str):
+    def predict_emotion_from_wav_file(self, wav_file_path: str) -> Literal['positive', 'negative']:
         start_time = time.time()
         try:
             res = get_predict_feat(wav_file_path)
             predictions = self.model.predict(res)
             logging.info(f"SER: pred: {predictions}, max: {np.argmax(predictions)}")
             y_pred = self.encoder.inverse_transform(predictions)
-            logging.info(f"SER: predict result: {y_pred[0][0]}")
-            print(f"SER: predict result: {y_pred[0][0]}")
-            if y_pred[0][0] == 'angry' or y_pred[0][0] == 'disgust' or y_pred[0][0] == 'fear' or  y_pred[0][0] == 'sad':
-                logging.info(f"SER: inference result: negative")
+            inference_result = y_pred[0][0]
+            logging.info(f"SER: inference result: {inference_result}")
+
+            if inference_result in ['angry', 'disgust', 'fear', 'sad']:
+                logging.info(f"SER: normalization: negative")
                 return 'negative'
-            elif y_pred[0][0] == 'happy' or y_pred[0][0] == 'surprise':
-                logging.info(f"SER: inference result: positive")
+            elif inference_result in ['happy', 'surprise', 'neutral', 'calm']:
+                logging.info(f"SER: normalization: positive")
                 return 'positive'
             else:
-                return 'none'
-            # inference_result = y_pred[0][0]
-            # logging.info(f"SER: inference result: {inference_result}")
-            # return inference_result
-
+                logging.info(f"SER: normalization: value out of range : {inference_result}")
+                logging.info(f"SER: normalization: falling back to    : negative")
+                return 'negative'
         finally:
             end_time = time.time()
             logging.info(f"SER: inference time: {end_time - start_time:.2f} seconds")
